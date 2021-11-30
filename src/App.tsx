01dea4c68cd49ab4303bf37a7e1e7d18f64a2b4e
gusmartins499 <gustavosm994@gmail.com>
2021-11-30 16:59:11 +0000

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { GlobalStyles } from "./styles/global";
+
 export function App() {
-  return <h1>DtMoney</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello World!</h1>
+    </>
+  );
 }
