8304a162a19550820f55d9722f6f056a9e9851b3
gusmartins499 <gustavosm994@gmail.com>
2021-12-03 13:14:37 +0000

    styled modal 2

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import Modal from "react-modal";
 
+import closeImg from "../../assets/close.svg";
 import { Container } from "./styles";
 
 interface IModalProps {
@@ -15,6 +16,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: IModalProps) {
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="fechar modal" />
+      </button>
+
       <Container>
         <h2>Cadastrar Transação</h2>
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -70,4 +70,17 @@ export const GlobalStyles = createGlobalStyle`
     position: relative;
     border-radius: 0.25rem;
   }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
 `;
